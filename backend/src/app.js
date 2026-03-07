b8aaac80c75e2026aa03597a31c9357819907cee
Krish Kumar <Krishsinghania6115@gmail.com>
2026-03-08 01:26:30 +0530

    update CORS origin to production server

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "ai-interview-planer.vercel.app",
     credentials: true
 }))
 
